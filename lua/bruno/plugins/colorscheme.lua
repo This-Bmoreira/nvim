f40306f12f9bd2cb87f5590ee6e0774e68a5a7de
This-Bmoreira <contatobrunosmoreira@gmail.com>
2024-06-14 19:08:48 -0300

    new theme

--- lua/bruno/plugins/colorscheme.lua
+++ lua/bruno/plugins/colorscheme.lua
@@ -1,12 +1,25 @@
+-- return {
+--   { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
+--   {
+--     "baliestri/aura-theme",
+--     lazy = false,
+--     priority = 1000,
+--     config = function(plugin)
+--       vim.opt.rtp:append(plugin.dir .. "/packages/neovim")
+--       vim.cmd([[colorscheme aura-dark]])
+--     end
+--   }
+-- }
+
 return {
-  { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
   {
-    "baliestri/aura-theme",
-    lazy = false,
-    priority = 1000,
-    config = function(plugin)
-      vim.opt.rtp:append(plugin.dir .. "/packages/neovim")
-      vim.cmd([[colorscheme aura-dark]])
-    end
-  }
+    "folke/tokyonight.nvim",
+    priority = 1000, 
+    config = function()
+      require("tokyonight").setup({
+        style = "night",
+      })
+      vim.cmd([[colorscheme tokyonight]])
+    end,
+  },
 }
